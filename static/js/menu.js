12b3ded73552db44b154e3b9933a889393127866
pedro092692 <pedro_092692@hotmail.com>
2024-08-01 16:47:51 -0400

    add mobile mene styles and js

--- static/js/menu.js
+++ static/js/menu.js
@@ -1,17 +1,46 @@
 function show_menu(){
-    let username = document.getElementById('username');
-    username.addEventListener('click', ()=>{
-        let menu = document.getElementById('menu');
-          menu.classList.add('test')
+    try{
+        let username = document.getElementById('username');
+        username.addEventListener('click', ()=>{
+            let window_width = window.innerWidth;
+            let menu = document.getElementById('menu');
+            let mobile_menu = document.getElementById('mobile-menu');
+            let nav = document.getElementById('nav');
+            if(window_width > 500){
+                menu.classList.add('test')
 
-        addEventListener('click', (event)=>{
-            if(!event.composedPath().includes(menu) && !event.composedPath().includes(username)){
-                menu.classList.remove('test');
+                close_menu(menu, window_width);
+            }else{
+                document.body.style.overflow = 'hidden';
+                username.style.display = 'none';
+                nav.style.display = 'none';
+                mobile_menu.classList.add('mobile-show');
+                let close = document.getElementById('close');
+                close_menu(close, window_width, mobile_menu, nav);
             }
         })
+    }catch{
+        //pass
+    }
 
+}
+
+function close_menu(button, window_width, mobile_menu){
+    addEventListener('click', (event)=>{
+         if(window_width > 500){
+             if(!event.composedPath().includes(button) && !event.composedPath().includes(username)){
+                    button.classList.remove('test');
+             }
+         }else{
+            console.log(event.composedPath())
+            if(event.composedPath().includes(button)){
+                nav.style.display = 'flex';
+                username.style.display = 'block';
+                document.body.style.overflow = 'auto';
+                mobile_menu.classList.remove('mobile-show');
+            }
+         }
     })
-
 }
 
 show_menu()
