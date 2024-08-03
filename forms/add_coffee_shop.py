112a8e549a45d6ff25de7432389d678cafc312a0
pedro092692 <pedro_092692@hotmail.com>
2024-08-03 00:06:21 -0400

    add whole form items in add coffee shop template

--- forms/add_coffee_shop.py
+++ forms/add_coffee_shop.py
@@ -9,10 +9,28 @@ class AddCoffeeShop(FlaskForm):
     description = StringField(label='Coffee Description', validators=[DataRequired()])
     location = StringField(label='Location', validators=[DataRequired()])
     wifi = BooleanField(label='Wifi', default='')
-    coffee = SelectField(label='Wifi', choices=[1, 2, 3, 4, 5], validators=[DataRequired()])
-    power = SelectField(label='Wifi', choices=[1, 2, 3, 4, 5], validators=[DataRequired()])
-    open = SelectField(label='Wifi', choices=[hour for hour in range(1, 12)], validators=[DataRequired()])
-    close = SelectField(label='Wifi', choices=[hour for hour in range(1, 12)], validators=[DataRequired()])
+    power_sockets = BooleanField(label='Power Sockets', default='')
+    quiet = BooleanField(label='Quiet', default='')
+    coffee_service = BooleanField(label='Coffee Service', default='')
+    food_service = BooleanField(label='Coffee Service', default='')
+    credit_card_service = BooleanField(label='Coffee Service', default='')
+    coffee = SelectField(label='Coffee score',
+                         choices=[('', 'Coffee Rating')] + [(f'{star}', f'{"⭐" * star}') for star in range(1, 6)],
+                         validators=[DataRequired()])
+    power = SelectField(label='Power score',
+                        choices=[('', 'Power Sockets Rating')] + [(f'{star}', f'{"⭐" * star}') for star in range(1, 6)],
+                         validators=[DataRequired()])
+    wifi_score = SelectField(label='Wifi score',
+                        choices=[('', 'WIFI Rating')] + [(f'{star}', f'{"⭐" * star}') for star in range(1, 6)],
+                         validators=[DataRequired()])
+    open = SelectField(label='Open', choices=[('', 'Open')] + [(f'{hour} AM', f'{hour} AM') for hour in range(1, 13)],
+                       validators=[DataRequired()])
+    close = SelectField(label='Open', choices=[('', 'Close')] + [(f'{hour} PM', f'{hour} PM') for hour in range(1, 13)],
+                       validators=[DataRequired()])
+
+    image_url = StringField(label='Image URL', validators=[DataRequired(), URL()])
+
+    submit = SubmitField(label='Add Coffee Shop')
 
 
 
